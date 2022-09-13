ac2e117e84f6a25531084fd24c7a7ac722ec5ccf
Lucas Andrade <lucasantos.dev@gmail.com>
2022-09-12 22:34:25 -0300

    config tailwindcss

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import './styles/main.css'
 
 export function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <div className="w-8 h-8 bg-violet-500"></div>
     </>
   )
 }
